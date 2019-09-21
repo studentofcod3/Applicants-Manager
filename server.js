3df030ddf9b1de8d406dc2388fde2c986a67e80f
ayyoub <ayyoub@hotmail.com>
2019-09-21 20:30:47 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const connectDB = require("./config/db");
 
+const path = require("path");
+
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const contacts = require("./routes/contacts");
@@ -12,14 +14,20 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API" })
-);
-
 // Define Routes
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/contacts", contacts);
+
+// Serve static assets (react) in production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
